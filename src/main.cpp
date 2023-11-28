87a00c55a482aebc285d4349f9861558b40e98a8
MooseWhoTinkers <moosewhotinkers@gmail.com>
2023-11-28 18:08:31 +0100

    Test with HID keyboard

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
+#include <Keyboard.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Keyboard.begin();
+
+  delay(2000);
+  Serial.print("Switch to Notepad!");
+  delay(3000);
+
+  Keyboard.print("Testing XIAO HID keyboard...1...2...3");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 }
